e1c82bf7809939a6fa518f5cc0713990cb0d2bba
Rabin <rabin@Rabins-MacBook-Air.local>
2024-01-20 07:48:26 +0545

    third

--- user.js
+++ user.js
@@ -1,5 +1,12 @@
-document.getElementById('abc').addEventListener('click', function() {
-    var userContainer = document.getElementById('userContainer');
-    userContainer.style.display = (userContainer.style.display === 'none' || userContainer.style.display === '') ? 'flex' : 'none';
-  });
-  
+document.getElementById('abc').addEventListener('click', function(event) {
+  var userContainer = document.getElementById('userContainer');
+  userContainer.style.display = (userContainer.style.display === 'none' || userContainer.style.display === '') ? 'flex' : 'none';
+  event.stopPropagation();
+});
+
+document.addEventListener('click', function(event) {
+  var userContainer = document.getElementById('userContainer');
+  if (event.target !== userContainer && !userContainer.contains(event.target)) {
+    userContainer.style.display = 'none';
+  }
+});
